7251cb4af0c9360a8c57e888b6639d2d9ed30026
rf <rf0444@gmail.com>
2013-04-28 17:45:01 +0900

    use Bacon.UI, use Bus in View

--- sample.js
+++ sample.js
@@ -1,33 +1,33 @@
 "use strict";
 $(function() {
-	function InputView(el, bus) {
-		var me = this;
-		me.el = el;
-		bus.plug(
-			me.el.asEventStream("change")
-				.map(function() { return me.el.val(); })
-				.merge(Bacon.once(me.el.val()))
-				.map(Number)
-		);
-	}
-	function OutputView(el, bus) {
-		var me = this;
-		me.el = el;
-		bus.assign(function(val) {
-			me.el.text(val);
-		});
+	function View(el) {
+		this.property = Bacon.UI.textFieldValue(el, 0).map(Number);
+		this.ibus = new Bacon.Bus();
+		this.obus = new Bacon.Bus();
+		this.ibus.plug(this.property.sampledBy(this.property));
+		this.obus.assign(function(val) { el.text(val); });
 	}
 	function Model() {
 		var me = this;
 		me.a = new Bacon.Bus();
 		me.b = new Bacon.Bus();
 		me.c = new Bacon.Bus();
-		me.c.plug(
-			Bacon.combineWith(function(a, b) { return a + b; }, me.a, me.b).changes()
-		);
+		var pc = Bacon.combineWith(function(a, b) { return a + b; }, me.a, me.b);
+		me.c.plug(pc.sampledBy(pc));
 	}
-	var model = new Model();
-	var a = new InputView($("#val1"), model.a);
-	var b = new InputView($("#val2"), model.b);
-	var c = new OutputView($("#val3"), model.c);
+	function App(conf) {
+		var model = conf.model;
+		var views = conf.views;
+		model.a.plug(views.a.ibus);
+		model.b.plug(views.b.ibus);
+		views.c.obus.plug(model.c);
+	}
+	new App({
+		model: new Model(),
+		views: {
+			a: new View($("#val1")),
+			b: new View($("#val2")),
+			c: new View($("#val3")),
+		},
+	});
 });
