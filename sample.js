9df57f86c441d9d209a77d9401ad6d0861cbe6cd
rf <rf0444@gmail.com>
2013-04-28 17:42:00 +0900

    use Bacon.js

--- sample.js
+++ sample.js
@@ -1,11 +1,33 @@
 "use strict";
 $(function() {
-	function onChange() {
-		var a = Number($("#val1").val());
-		var b = Number($("#val2").val());
-		var c = a + b;
-		$("#val3").text(c);
+	function InputView(el, bus) {
+		var me = this;
+		me.el = el;
+		bus.plug(
+			me.el.asEventStream("change")
+				.map(function() { return me.el.val(); })
+				.merge(Bacon.once(me.el.val()))
+				.map(Number)
+		);
 	}
-	$("#val1").change(onChange);
-	$("#val2").change(onChange);
+	function OutputView(el, bus) {
+		var me = this;
+		me.el = el;
+		bus.assign(function(val) {
+			me.el.text(val);
+		});
+	}
+	function Model() {
+		var me = this;
+		me.a = new Bacon.Bus();
+		me.b = new Bacon.Bus();
+		me.c = new Bacon.Bus();
+		me.c.plug(
+			Bacon.combineWith(function(a, b) { return a + b; }, me.a, me.b).changes()
+		);
+	}
+	var model = new Model();
+	var a = new InputView($("#val1"), model.a);
+	var b = new InputView($("#val2"), model.b);
+	var c = new OutputView($("#val3"), model.c);
 });
