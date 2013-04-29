51a731b1112646d230646531589e0d0e1821eb94
rf <rf0444@gmail.com>
2013-04-29 13:48:10 +0900

    rename ibus <-> obus, make c inputable

--- bacon/sample.js
+++ bacon/sample.js
@@ -1,26 +1,42 @@
 "use strict";
 $(function() {
 	function View(el) {
-		this.property = Bacon.UI.textFieldValue(el, 0).map(Number);
-		this.ibus = new Bacon.Bus();
-		this.obus = new Bacon.Bus();
-		this.ibus.plug(this.property.sampledBy(this.property));
-		this.obus.assign(function(val) { el.text(val); });
+		var me = this;
+		me.property = Bacon.UI.textFieldValue(el, 0).map(Number);
+		
+		me.obus = new Bacon.Bus();
+		me.obus.plug(me.property.sampledBy(me.property));
+		
+		me.ibus = new Bacon.Bus();
+		me.ibus.assign(function(val) { el.val(val); });
 	}
 	function Model() {
 		var me = this;
 		me.a = new Bacon.Bus();
 		me.b = new Bacon.Bus();
 		me.c = new Bacon.Bus();
-		var pc = Bacon.combineWith(function(a, b) { return a + b; }, me.a, me.b);
-		me.c.plug(pc.sampledBy(pc));
+		me.c.plug(
+			Bacon.combineWith(function(a, b) { return a + b; }, me.a, me.b)
+				.sampledBy(me.a.merge(me.b))
+				.skipDuplicates()
+		);
+		me.b.plug(
+			Bacon.combineWith(function(a, c) { return c - a; }, me.a, me.c)
+				.sampledBy(me.c)
+				.skipDuplicates()
+		);
 	}
 	function App(conf) {
 		var model = conf.model;
 		var views = conf.views;
-		model.a.plug(views.a.ibus);
-		model.b.plug(views.b.ibus);
-		views.c.obus.plug(model.c);
+		// output first
+		views.a.ibus.plug(model.a);
+		views.b.ibus.plug(model.b);
+		views.c.ibus.plug(model.c);
+		// input last
+		model.a.plug(views.a.obus);
+		model.b.plug(views.b.obus);
+		model.c.plug(views.c.obus);
 	}
 	new App({
 		model: new Model(),
